b5c30eeed0464352388cea98c3bb3ef310f61306
Eugene Lanets <swarrt@yandex.ru>
2021-08-21 22:36:22 +0300

    feat: add placeholders

--- src/components/SignForm/SignFormField/SignFormField.js
+++ src/components/SignForm/SignFormField/SignFormField.js
@@ -4,7 +4,7 @@ import './SignFormField.scss';
 const SignFormField = ({ label, inputType, fieldId }) => (
   <div className="sign-form__field sign-form-field">
     <label className="sign-form-field__label" htmlFor={fieldId}>{label}</label>
-    <input type={inputType} className="sign-form-field__input sign-form__text" id={fieldId} required />
+    <input type={inputType} className="sign-form-field__input sign-form__text" id={fieldId} required placeholder={label} />
     <p className="sign-form-field__error">Что-то пошло не так</p>
   </div>
 );
