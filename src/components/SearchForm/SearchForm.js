4b562c823e910f0e4bd2f970edb3aa6793f4d754
Eugene Lanets <swarrt@yandex.ru>
2021-08-21 13:49:32 +0300

    feat: add Register, Login, SearchForm inputs hover and focus

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import FilterCheckbox from './FilterCheckbox/FilterCheckbox';
 import './SearchForm.scss';
 
-const SearchForm = () => (
-  <form className="search-form">
-    <div className="search-form__field">
-      <input
-        type="text"
-        className="search-form__input"
-        placeholder="Фильм"
-      />
-      <button type="submit" className="search-form__submit">Найти</button>
-    </div>
-    <FilterCheckbox />
-  </form>
-);
+const SearchForm = () => {
+  const formField = useRef();
+  const handleInputFocus = (ref, className) => {
+    ref.current.classList.add(className);
+  };
+  const handleInputBlur = (ref, className) => {
+    ref.current.classList.remove(className);
+  };
+
+  return (
+    <form className="search-form">
+      <label htmlFor="search" className="search-form__field" ref={formField}>
+        <input
+          id="search"
+          type="text"
+          className="search-form__input"
+          placeholder="Фильм"
+          onFocus={() => handleInputFocus(formField, 'search-form__field_focus')}
+          onBlur={() => handleInputBlur(formField, 'search-form__field_focus')}
+        />
+        <button type="submit" className="search-form__submit">Найти</button>
+      </label>
+      <FilterCheckbox />
+    </form>
+  );
+};
 
 export default SearchForm;
